d32d0591829d20e9c67930f7b05248aa8ad8a18c
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-18 19:07:50 +0200

    feat: no app module 😱

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+export const SwitchDeps = [NgSwitch, NgSwitchDefault, NgSwitchCase] as const;
 
 @Component({
+  standalone: true,
+  imports: [SwitchDeps, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fic-ng_dev-23_04';
